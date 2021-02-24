8d573a0a90266df26ef1dbba6639445089736172
Jose Miralles <josemiralles1991@gmail.com>
2021-02-24 17:39:37 -0500

    First example done

--- GettingStarted/1-3/main.cpp
+++ GettingStarted/1-3/main.cpp
@@ -6,6 +6,12 @@
 using namespace std;
 
 int main() {
-
+	setiosflags(ios::left);
+	for (int i = 0; i <= 6; i++) {
+		for (int x = 0; x <= 4; x++) {
+			cout << setw(17) << "Hello World!";
+		}
+		cout << endl;
+	}
 	return 0;
 }
